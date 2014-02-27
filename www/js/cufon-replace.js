14b5d7f43072942b2df1937dea9df2ba15577711
Li He <helionitial@me.com>
2014-02-26 20:38:23 -0600

    init

---
+++ www/js/cufon-replace.js
@@ -0,0 +1,2 @@
+Cufon.replace('.text, h4, .title1, .data, h2, .link1, .block3-title, .block3-text, .box-link, .text3, .block4-text, .link5, .title2, h1, .text5, h3, h5, h6 ', { fontFamily: 'rolina', hover:true });
+
